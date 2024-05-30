5122d6788f1746c414b1e91be908e4f6e11f5bf3
Max Haney <60326999+mchaney-dev@users.noreply.github.com>
2024-05-30 09:51:20 -0400

    Update scrape.py

--- backend/scrape.py
+++ backend/scrape.py
@@ -3,9 +3,6 @@ import time
 import json
 
 def scrape_rooms(url: str) -> tuple:
-    # debug
-    print('Sleeping for 10 seconds...')
-    
     time.sleep(10) # Be nice to the server
     response = json.loads(requests.get(url).text)
     curr_page = response.get('page')
