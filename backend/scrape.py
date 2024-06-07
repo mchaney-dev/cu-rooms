fe0f64770794cef90a5588c4dace51bd1c708d53
Max Haney <60326999+mchaney-dev@users.noreply.github.com>
2024-06-07 10:01:24 -0400

    refactored code

--- backend/scrape.py
+++ backend/scrape.py
@@ -2,18 +2,18 @@ import requests
 import time
 import json
 
-def scrape_rooms(url: str, verbose=False) -> tuple:
+def scrape_rooms(url: str, sleep=10, verbose=False) -> tuple:
     if verbose:
         print('[DEBUG] Sleeping for 10 seconds...')
-    time.sleep(10) # Be nice to the server
+    time.sleep(sleep)
     response = json.loads(requests.get(url).text)
     curr_page = response.get('page')
     total_pages = response.get('rows')[0]['pages']
     return response.get('rows'), curr_page, total_pages
 
-def scrape_events(url: str, verbose=False):
+def scrape_events(url: str, sleep=10, verbose=False):
     if verbose:
         print('[DEBUG] Sleeping for 10 seconds...')
-    time.sleep(10) # Be nice to the server
+    time.sleep(sleep)
     response = json.loads(requests.get(url).text)
     return response.get('root').get('events')
---
+++ main.py
@@ -0,0 +1,4 @@
+from backend.driver import Driver
+
+if __name__ == '__main__':
+    driver = Driver(verbose=True)
