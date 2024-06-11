0b665fe3419e752fb29b3debb845d5b978dcf488
Max Haney <60326999+mchaney-dev@users.noreply.github.com>
2024-06-11 14:10:10 -0400

    switched to flask endpoint

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 from backend.driver import Driver
+import flask
+import json
+
+driver = Driver(verbose=True)
+app = flask.Flask('CU Rooms')
+
+@app.route("/")
+def home():
+    return "Home"
+
+@app.route("/rooms", methods=["GET"])
+def get_rooms():
+    return json.dumps(driver.get_rooms())
+
+@app.route("/events", methods=["GET"])
+def get_events():
+    return json.dumps(driver.get_events())
 
 if __name__ == '__main__':
-    driver = Driver(verbose=True)
+    app.run("localhost", 5000)
